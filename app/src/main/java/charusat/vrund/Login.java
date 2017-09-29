0a8d1b76301e125bd5a4ec803c487835eb6d8af9
yasheth <yasheth77@gmail.com>
2017-09-29 22:22:35 +0530

    Organizer Fragment Added

--- app/src/main/java/charusat/vrund/Login.java
+++ app/src/main/java/charusat/vrund/Login.java
@@ -5,10 +5,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class Login extends AppCompatActivity {
 
     Button register, login;
+    TextView rollNumber, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,10 +22,22 @@ public class Login extends AppCompatActivity {
         register = (Button) findViewById(R.id.bt_register);
         login = (Button) findViewById(R.id.bt_login);
 
+        rollNumber = (TextView) findViewById(R.id.et_rollno);
+        password = (TextView) findViewById(R.id.et_password);
+
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(Login.this, MainActivity.class);
+
+                String enteredRollNumber = rollNumber.getText().toString();
+                String enteredPassword = password.getText().toString();
+                Intent i;
+                if(enteredRollNumber.equals("14IT120")){
+                    i = new Intent(Login.this, MainActivity_Organiser.class);
+                }
+                else{
+                    i = new Intent(Login.this, MainActivity.class);
+                }
                 startActivity(i);
                 finish();
             }
---
+++ app/src/main/java/charusat/vrund/Scanner_Fragment.java
@@ -0,0 +1,32 @@
+package charusat.vrund;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+
+public class Scanner_Fragment extends Fragment {
+
+    public Scanner_Fragment(){
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
