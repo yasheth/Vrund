81a1a87cb1a61bb2bcd46fe88387c4a9ef0f2d98
umangpatel98 <umangpatel1947@gmail.com>
2017-10-01 21:40:01 +0530

    Updated content and layout......

--- app/src/main/java/charusat/vrund/home/HomeRecyclerAdapter.java
+++ app/src/main/java/charusat/vrund/home/HomeRecyclerAdapter.java
@@ -1,7 +1,6 @@
 package charusat.vrund.home;
 
 import android.support.v7.widget.CardView;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,6 +17,7 @@ import charusat.vrund.R;
 
 /**
  * Created by iharsh on 28/9/17.
+ * Adapter class for Home fragment layout
  */
 
 public class HomeRecyclerAdapter extends RecyclerView.Adapter<HomeRecyclerAdapter.ItemsHolder> {
@@ -42,25 +42,12 @@ public class HomeRecyclerAdapter extends RecyclerView.Adapter<HomeRecyclerAdapte
         CardItem currentItem = cardItemArrayList.get(position);
         holder.homeCardTextView.setText(currentItem.getTextLabel());
         Picasso.with(holder.homeCardImageView.getContext())
-                .load(currentItem.getImageResourceUrl())
-                .placeholder(currentItem.getImageResourceId())
+                .load(currentItem.getImageResourceId())
+                .placeholder(R.drawable.placeholder_image_loading)
                 .into(holder.homeCardImageView);
 
         if (position == (cardItemArrayList.size() - 1)) {
-            AppMember[] appMembers = new AppMember[5];
-            appMembers[0] = new AppMember(R.drawable.app_harsh_shah, R.string.app_member_harsh_shah);
-            appMembers[1] = new AppMember(R.drawable.app_yash_sheth, R.string.app_member_yash_sheth);
-            appMembers[2] = new AppMember(R.drawable.app_umang_patel, R.string.app_member_umang_patel);
-            appMembers[3] = new AppMember(R.drawable.app_yash_sodha, R.string.app_member_yash_sodha);
-            appMembers[4] = new AppMember(R.drawable.app_mitkumar_patel, R.string.app_member_mit_patel);
-
             holder.appTeamCardView.setVisibility(View.VISIBLE);
-            holder.appTeamRecyclerView.setHasFixedSize(true);
-            LinearLayoutManager manager = new LinearLayoutManager(holder.appTeamCardView.getContext());
-            holder.appTeamRecyclerView.setLayoutManager(manager);
-            AppTeamRecyclerAdapter adapter = new AppTeamRecyclerAdapter(appMembers);
-            holder.appTeamRecyclerView.setAdapter(adapter);
-
             holder.kalashLayout.setVisibility(View.VISIBLE);
         } else {
             holder.kalashLayout.setVisibility(View.GONE);
@@ -83,7 +70,6 @@ public class HomeRecyclerAdapter extends RecyclerView.Adapter<HomeRecyclerAdapte
     class ItemsHolder extends RecyclerView.ViewHolder {
 
         CardView appTeamCardView;
-        RecyclerView appTeamRecyclerView;
         LinearLayout kalashLayout;
         ImageView homeCardImageView;
         TextView homeCardTextView;
@@ -91,7 +77,6 @@ public class HomeRecyclerAdapter extends RecyclerView.Adapter<HomeRecyclerAdapte
         ItemsHolder(View itemView) {
             super(itemView);
             appTeamCardView = (CardView) itemView.findViewById(R.id.home_app_team_card_view);
-            appTeamRecyclerView = (RecyclerView) itemView.findViewById(R.id.home_app_team_recycler_view);
             kalashLayout = (LinearLayout) itemView.findViewById(R.id.kalash_layout);
             homeCardImageView = (ImageView) itemView.findViewById(R.id.home_image_card_image_view);
             homeCardTextView = (TextView) itemView.findViewById(R.id.home_text_card_text_view);
