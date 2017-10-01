f7bd9f1f86bd5953b8b55212815dc649a3ea53e3
yasheth <yasheth77@gmail.com>
2017-10-01 17:27:45 +0530

    PackageUpdate

---
+++ app/src/main/java/charusat/vrund17/User.java
@@ -0,0 +1,33 @@
+package charusat.vrund17;
+
+/**
+ * Created by Harsh Shah on 29/09/2017.
+ */
+
+public class User {
+
+    public String email;
+    public String name;
+    public String phone;
+    public String role;
+    public String gender;
+    public  boolean ioc;
+    public  boolean organiser;
+    public String p_id;
+
+    User(){
+
+    }
+
+    User(String email, String name, String phone, String role, String gender, boolean ioc, String p_id, boolean organiser) {
+        this.email = email;
+        this.name = name;
+        this.phone = phone;
+        this.role = role;
+        this.gender = gender;
+        this.ioc = ioc;
+        this.p_id = p_id;
+        this.organiser = organiser;
+    }
+
+}
